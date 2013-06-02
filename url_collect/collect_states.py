0591fb24dd763f3f5d28879306c66478ddf5cf87
John Lockwood <johnwlockwood@gmail.com>
2013-06-02 00:41:41 -0700

    Fix pep8 issues and streamline state collection function

--- url_collect/collect_states.py
+++ url_collect/collect_states.py
@@ -2,12 +2,21 @@ from muni_data_client import _collect_states_data
 from muni_data_client import assign_guids
 from muni_data_client import write_state_data
 
+
 def main():
-    state_list = ['al', 'ak', 'az', 'ar', 'ca', 'co', 'ct', 'de', 'fl', 'ga', 'hi', 'id', 'il', 'in', 'ia', 'ks', 'ky', 'la', 'me', 'md', 'ma', 'mi', 'mn', 'ms', 'mo', 'mt', 'ne', 'nv', 'nh', 'nj', 'nm', 'ny', 'nc', 'nd', 'oh', 'ok', 'or', 'pa', 'ri', 'sc', 'sd', 'tn', 'tx', 'ut', 'vt', 'va', 'wa', 'wv', 'wi', 'wy', 'as', 'dc', 'fm', 'gu', 'mh', 'mp', 'pw', 'pr', 'vi']
+    state_list = ['al', 'ak', 'az', 'ar', 'ca', 'co',
+                  'ct', 'de', 'fl', 'ga', 'hi', 'id',
+                  'il', 'in', 'ia', 'ks', 'ky', 'la',
+                  'me', 'md', 'ma', 'mi', 'mn', 'ms',
+                  'mo', 'mt', 'ne', 'nv', 'nh', 'nj',
+                  'nm', 'ny', 'nc', 'nd', 'oh', 'ok',
+                  'or', 'pa', 'ri', 'sc', 'sd', 'tn',
+                  'tx', 'ut', 'vt', 'va', 'wa', 'wv',
+                  'wi', 'wy', 'as', 'dc', 'fm', 'gu',
+                  'mh', 'mp', 'pw', 'pr', 'vi']
     states_data = _collect_states_data(state_list)
-    for state_data in states_data:
-        data_identified = assign_guids(state_data[1])
-        write_state_data(state_data[0], list(data_identified), "out")
+    for state, data in states_data:
+        write_state_data(state, list(assign_guids(data)), "out")
 
 
 if __name__ == '__main__':
--- url_collect/muni_data_client/__init__.py
+++ url_collect/muni_data_client/__init__.py
@@ -33,8 +33,10 @@ def _assign_guid(city_or_county_data):
     data['guid'] = uuid.uuid4().hex
     return data
 
+
 def assign_guids(city_county_data_of_state):
     return imap(_assign_guid, city_county_data_of_state)
+
 
 def write_state_data(state, data, project_dir=""):
     try:
