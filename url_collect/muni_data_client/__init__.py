67c46aac2ddb9c302a7d4009ceb1a73caa30cd15
John Lockwood <johnwlockwood@gmail.com>
2013-06-01 19:37:43 -0700

    Add state data collector
    Add resulted data collection

--- url_collect/muni_data_client/__init__.py
+++ url_collect/muni_data_client/__init__.py
@@ -1,7 +1,32 @@
 from itertools import imap
+from itertools import ifilter
 import uuid
 import os
 import json
+import requests
+
+
+def _collect_url_json(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            return data
+        except ValueError:
+            pass
+
+def _collect_state_data(state):
+    url = "http://api.sba.gov/geodata/" \
+          "city_county_data_for_state_of/{0}.json".format(state)
+    data = _collect_url_json(url)
+    if data:
+        return (state, data)
+
+
+def _collect_states_data(states):
+    return ifilter(lambda x: x is not None,
+                   imap(_collect_state_data, states))
+
 
 def _assign_guid(city_or_county_data):
     data = city_or_county_data.copy()
